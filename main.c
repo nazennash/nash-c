f76c1a93ae10ad633300820697cb5d3c85e4d8a9
nazennash <nazennash120@gmail.com>
2024-11-14 13:46:38 +0300

    typedef

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-struct rectangle
+typedef struct
 {
     int length;
     int width;
-};
+} rectangle;
 
 int main()
 {
-    struct rectangle r1 = {10, 20};
-    struct rectangle r2 = {30, 40};
+    rectangle r1 = {10, 20};
+    rectangle r2 = {30, 40};
 
     printf("Area of r1: %d\n", r1.length * r1.width);
     printf("Area of r2: %d\n", r2.length * r2.width);
